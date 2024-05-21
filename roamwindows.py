d6a682648e18a552871dccf488400670ca6ab3a0
Michał Kowalik (kowalikm) <kowalikm@cisco.com>
2024-05-21 16:33:53 +0200

    split functions per operating system

--- roamwindows.py
+++ roamwindows.py
@@ -11,61 +11,107 @@ netpacket.net/2020/06/client-roaming-and-scripts/
 
 from datetime import datetime
 import subprocess
+import platform
 import re
 import sys
 import time
 
 from colorama import init, Fore
 
-output_file_name = 'wifiroam.csv'
-output_delay = 0.3
-show_as_rssi = False
+
+class scan:
+
+    roam_counter = 0
+    previous_bssid = None
+    scan_output = ""
+    output_file_name = 'wifiroam.csv'
+    output_delay = 0.3
+    show_as_rssi = False
+
+
+def run_windows():
+
+    try:
+        timestamp = str(datetime.now())[:-7]
+        output = subprocess.check_output('netsh wlan show interfaces').decode('ascii')        
+        ssid = re.search(r'(SSID.+?:\s)(.+)', output).group(2).rstrip()
+        bssid = re.search(r'(BSSID.+?:\s)(.+)', output).group(2).rstrip()
+        channel = re.search(r'(Channel.+?:\s)(.+)', output).group(2).rstrip()
+        band = re.search(r'(Band\s+:\s)(.+)', output).group(2).rstrip()
+        rxrate = re.search(r'(Receive rate.+?:\s)(.+)', output).group(2).rstrip()
+        txrate = re.search(r'(Transmit rate.+?:\s)(.+)', output).group(2).rstrip()
+        signal = re.search(r'(Signal.+?:\s)(.+)', output).group(2).rstrip()
+                
+        #show_as_rssi - attempts to convert Windows signal strength % into dBm - believe this at your own risk
+        if scan.show_as_rssi:
+            signal = f"-{str(100-round(int(signal[:-1])/2))}dBm"
+
+    except AttributeError:
+        print('Data unavailable')
+    else:
+        if bssid != scan.previous_bssid:
+            scan.previous_bssid = bssid
+            scan.roam_counter += 1
+        if scan.roam_counter % 2 == 0:
+            bss_colour = Fore.CYAN
+        else:
+            bss_colour = Fore.YELLOW
+
+        print(timestamp[11:], Fore.GREEN + "SSID:", ssid, Fore.GREEN + "BSSID:", bss_colour + bssid, 
+              Fore.GREEN + "Channel:", channel, Fore.GREEN + "Band:", band, Fore.GREEN + "Signal:", signal, 
+              Fore.GREEN + "RX(Mbps):", rxrate, Fore.GREEN + "TX(Mbps):", txrate)
+        
+        scan.scan_output = f"{timestamp[:11]},{timestamp[11:]},{ssid},{bssid},{channel},{band},{signal},{rxrate},{txrate}\n"
+
+
+def run_apple():
+    
+    try:
+        timestamp = str(datetime.now())[:-7]
+        output = subprocess.check_output("profile sudo wdutil info").decode("ascii")
+        ssid = re.search(r'(SSID\s+:\s)(.+)', output).group(2).rstrip()
+        bssid = re.search(r'(BSSID\s+:\s)(.+)', output).group(2).rstrip()
+        channel = re.search(r'(Channel\s+:\s)(.+)', output).group(2).rstrip()
+        txrate = re.search(r'(Tx Rate\s+:\s)(.+)', output).group(2).rstrip()
+        rssi = re.search(r'(RSSI\s+:\s)(.+)', output).group(2).rstrip()
+
+    except AttributeError:
+        print('Data unavailable')
+    else:
+        if bssid != scan.previous_bssid:
+            scan.previous_bssid = bssid
+            scan.roam_counter += 1
+        if scan.roam_counter % 2 == 0:
+            bss_colour = Fore.CYAN
+        else:
+            bss_colour = Fore.YELLOW    
+
+        print(timestamp[11:], Fore.GREEN + "SSID:", ssid, Fore.GREEN + "BSSID:", bss_colour + bssid, 
+              Fore.GREEN + "Channel:", channel, Fore.GREEN + "RSSI:", rssi, Fore.GREEN + "TX(Mbps):", txrate)
+
+        scan.scan_output = f"{timestamp[:11]},{timestamp[11:]},{ssid},{bssid},{channel},{rssi},{txrate}\n"
 
 
 def run():
 
-    roam_counter = 0
-    previous_bssid = None
+    os = platform.system()
     try:
-        output_file = open(output_file_name, "w+")
+        output_file = open(scan.output_file_name, "w+")
     except PermissionError:
-        print('Error writing file')
+        print("Error writing file")
         sys.exit()
     try:
         while True:
-            try:
-                timestamp = str(datetime.now())[:-7]
-                output = subprocess.check_output('netsh wlan show interfaces').decode('ascii')
+            if os == "Windows":
+                run_windows()
+            elif os =="Darwin":
+                pass
+                #Apple functionality not implemented
+                #run_apple()
             
-                ssid = re.search(r'(SSID.+?:\s)(.+)', output).group(2).rstrip()
-                bssid = re.search(r'(BSSID.+?:\s)(.+)', output).group(2).rstrip()
-                channel = re.search(r'(Channel.+?:\s)(.+)', output).group(2).rstrip()
-                band = re.search(r'(Band\s+:\s)(.+)', output).group(2).rstrip()
-                rxrate = re.search(r'(Receive rate.+?:\s)(.+)', output).group(2).rstrip()
-                txrate = re.search(r'(Transmit rate.+?:\s)(.+)', output).group(2).rstrip()
-                signal = re.search(r'(Signal.+?:\s)(.+)', output).group(2).rstrip()
-                
-                #show_as_rssi - attempts to convert Windows signal strength % into dBm - believe this at your own risk
-                if show_as_rssi:
-                    signal = f"-{str(100-round(int(signal[:-1])/2))}dBm"
-
-            except AttributeError:
-                print('Data unavailable')
-            else:
-                if bssid != previous_bssid:
-                    previous_bssid = bssid
-                    roam_counter += 1
-                if roam_counter % 2 == 0:
-                    bss_colour = Fore.CYAN
-                else:
-                    bss_colour = Fore.YELLOW
-
-                print(timestamp[11:], Fore.GREEN + "SSID:", ssid, Fore.GREEN + "BSSID:", bss_colour + bssid, 
-                      Fore.GREEN + "Channel:", channel, Fore.GREEN + "Band:", band, Fore.GREEN + "Signal:", signal, 
-                      Fore.GREEN + "RX(Mbps):", rxrate, Fore.GREEN + "TX(Mbps):", txrate)
-                output_file.write(f"{timestamp[:11]},{timestamp[11:]},{ssid},{bssid},{channel},{band},{signal},{rxrate},{txrate}\n")
-
-            time.sleep(output_delay)
+            output_file.write(scan.scan_output)
+            time.sleep(scan.output_delay)
+    
     except KeyboardInterrupt:
         output_file.close()
         sys.exit()
